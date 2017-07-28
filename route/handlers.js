6be180b575ef8c44522515cbd36e8781248a7169
Ahmad Anvari <ahmad.anv@gmail.com>
2017-07-28 16:25:28 +0430

    Adding handlers

--- route/handlers.js
+++ route/handlers.js
@@ -1,13 +1,31 @@
 var mqtt_router = require('./mqtt-router');
+var moment = require('moment');
+
+const db = require('./../database/database');
 
 mqtt_router.on('log', (body, request_token)=>{
+    console.log(process.hrtime()[1]);
+    console.log(moment().unix());
     console.log('log handler is called');
 });
 
 mqtt_router.on('get', (body, request_token)=>{
+    const query = "SELECT * FROM emp;";
+    console.log(db.cassandraClient);
+    db.cassandraClient().execute(query, (error, result)=>{
+        if (error){
+            console.log('this error');
+            console.error(error);
+            console.log('that error');
+        } else
+            console.log(result.rows[0])
+    });
     console.log('get handler is called');
 });
 
 mqtt_router.on('unknown', (body, request_token)=>{
     console.error('The unknown action is called');
 });
+
+
+module.exports = mqtt_router;
