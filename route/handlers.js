abcc888b5794f65c2ddf230614f0a5a1677659d3
Ahmad Anvari <ahmad.anv@gmail.com>
2017-07-28 20:01:24 +0430

    insert log for database

--- route/handlers.js
+++ route/handlers.js
@@ -4,12 +4,12 @@ var moment = require('moment');
 const db = require('./../database/database');
 
 mqtt_router.on('log', (body, request_token)=>{
-    const query = "INSERT INTO logs (timestamp, nanoseconds, device_id, status) VALUES";
+    const query = "INSERT INTO logs (timestamp, nanoseconds, device_id, states) VALUES (?, ?, ?, ?)";
     let nanoseconds = process.hrtime()[1];
     let timestamp = moment().unix();
-    let parameter = [timestamp, nanoseconds, body.device_id, status];
-    db.cassandraClient().execute(query, body, (error)=>{
-        console.error(error);
+    let parameter = [timestamp, nanoseconds, body.device_id, body.states];
+    db.cassandraClient().execute(query, parameter, { prepare: true }, (error)=>{
+        if (error) console.error(error);
     });
     console.log('log handler is called');
 });
