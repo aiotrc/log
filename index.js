948094438491c5e49a0eb404d47be0f2e8d0a2b1
amirmh <amir.haghollahi@gmail.com>
2017-08-01 16:05:27 +0430

    mqtt port added

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const config = require('./config');
 
 var mqtt_router = require('./route/handlers');
 
-const client  = mqtt.connect('mqtt://' + config.mqtt.message_broker_url);
+const client  = mqtt.connect('mqtt://' + config.mqtt.message_broker_url + ":" + config.mqtt.message_broker_port);
 const database = require('./database/database');
 
 
 client.on('connect', () => {
     client.subscribe('log');
-	client.publish('log',JSON.stringify({"action":"log", "request_token":"1","body":{"states":{"temp":"12.3","humidity":"15.5"},"device_id":"11"}}));
     database.cassandraConnect().
     then((cassandraClient)=>console.log('Successfully connected to cassandra')).
     error((error)=> console.log(error));
--- route/handlers.js
+++ route/handlers.js
@@ -4,23 +4,14 @@ var moment = require('moment');
 const db = require('./../database/database');
 
 mqtt_router.on('log', (body, request_token)=>{
-<<<<<<< HEAD
     const query = "INSERT INTO logs (timestamp, nanoseconds, device_id, states) VALUES (?,?,?,?)";
     let nanoseconds = process.hrtime()[1];
     let timestamp = moment().unix();
-    let parameter = [timestamp, nanoseconds, body.device_id, states];
-    db.cassandraClient().execute(query, body, (error)=>{
-        console.error(error);
-=======
-    const query = "INSERT INTO logs (timestamp, nanoseconds, device_id, states) VALUES (?, ?, ?, ?)";
-    let nanoseconds = process.hrtime()[1];
-    let timestamp = moment().unix();
     let parameter = [timestamp, nanoseconds, body.device_id, body.states];
-    db.cassandraClient().execute(query, parameter, { prepare: true }, (error)=>{
-        if (error) console.error(error);
->>>>>>> e1a4be9a17dd937a978ffa76cf8e3c57f36d827f
+    db.cassandraClient().execute(query, parameter, (error)=>{
+        console.error(error);
+    	console.log('log handler is called');
     });
-    console.log('log handler is called');
 });
 
 mqtt_router.on('get', (body, request_token)=>{
